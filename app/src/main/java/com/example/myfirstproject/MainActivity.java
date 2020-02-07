ee39fa69f2aad7cb5c4d58f8c2135d4a383f0f3b
muhammad_maulana <maulanajuni30@gmail.com>
2020-02-07 10:53:11 +0700

    update pertama kali 1. mengubah main activity 2. activity_main

--- app/src/main/java/com/example/myfirstproject/MainActivity.java
+++ app/src/main/java/com/example/myfirstproject/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //dodol
+        //dodo
 
         Button btnPindah = findViewById(R.id.btn);
         btnPindah.setOnClickListener(new View.OnClickListener() {
