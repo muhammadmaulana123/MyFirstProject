7a6fb4c7d30a51907ef82264e8c939c5a4d4a8ba
muhammad_maulana <maulanajuni30@gmail.com>
2020-02-07 09:25:04 +0700

    share project pertama kali

---
+++ app/src/main/java/com/example/myfirstproject/Main2Act.java
@@ -0,0 +1,15 @@
+package com.example.myfirstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
