6badf88a8c47ab4b20e0af50a8ca3086173e05d8
muhammad_maulana <maulanajuni30@gmail.com>
2020-02-14 01:36:54 +0700

    ini ktp

---
+++ app/src/main/java/com/example/myfirstproject/Layout_ktp.java
@@ -0,0 +1,14 @@
+package com.example.myfirstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout_ktp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_ktp);
+    }
+}
